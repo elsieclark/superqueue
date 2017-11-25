b88ee4454dc2136b929a4ff8c875fa8276dd63f8
willdavidc <willdavidc@gmail.com>
2017-11-24 21:30:21 -0500

    Add testing helpers

---
+++ tests/helpers/queueItem.js
@@ -0,0 +1,139 @@
+'use strict';
+const _            = require('lodash');
+const EventEmitter = require('events').EventEmitter;
+
+const States = {
+    unqueued: 0,
+    queued: 1,
+    executing: 2,
+    completed: 3,
+};
+
+const State = function() {
+    let state = States.unqueued;
+    this.getState = () => {
+        return state;
+    };
+    this.is = (otherState) => {
+        return state === otherState;
+    };
+    this.isAtLeast = (otherState) => {
+        return state >= otherState;
+    };
+    this.setState = (newState) => {
+        state = Math.max(newState, state);
+    };
+};
+
+const QueueItem = function(queue) {
+    let resolve;
+    let reject;
+    let state = new State();
+    let resolved = false;
+    let rejected = false;
+
+    const emitter = new EventEmitter();
+
+    this.isExecuting = () => {
+        return state.is(States.executing);
+    };
+
+    this.isResolved = () => {
+        return resolved;
+    };
+
+    this.isRejected = () => {
+        return rejected;
+    };
+
+    this.resolve = () => {
+        if (!this.isExecuting()) {
+            return false;
+        }
+        if (!resolve) {
+            return false;
+        }
+        resolve();
+        state.setState(States.completed);
+        resolved = true;
+        return true;
+    };
+
+    this.reject = () => {
+        if (!isExecuting) {
+            return false;
+        }
+        if (!reject) {
+            return false;
+        }
+        reject();
+        state.setState(States.completed);
+        rejected = true;
+        return true;
+    };
+
+    this.push = (options) => {
+        emitter.emit('pushed');
+        const params = [() => {
+            return new Promise((res, rej) => {
+                state.setState(States.executing);
+                emitter.emit('executed');
+                resolve = res;
+                reject = rej;
+            });
+        }];
+        if (!_.isUndefined(options)) {
+            params.unshift(options);
+        }
+        queue.push(...params)
+            .then((result) => {
+                emitter.emit('finished');
+                return result;
+            })
+            .catch((err) => {
+                emitter.emit('finished');
+                throw err;
+            });
+        state.setState(States.queued);
+    };
+
+    this.waitFor = (eventType, timeout) => {
+        const eventTypes = ['pushed', 'executed', 'finished'];
+        if (!_.includes(eventTypes, eventType)) {
+            throw new Error(`Test Error: Waiting for unknown event type ${eventType}`);
+        }
+        if (eventType === 'pushed' && state.isAtLeast(States.queued)) {
+            return Promise.resolve();
+        }
+        if (eventType === 'executed' && state.isAtLeast(States.executing)) {
+            return Promise.resolve();
+        }
+        if (eventType === 'finished' && state.isAtLeast(States.completed)) {
+            return Promise.resolve();
+        }
+        return new Promise((res, rej) => {
+            let timer;
+            emitter.once(eventType, () => {
+                res();
+                clearTimeout(timer);
+            });
+            if (!_.isFinite(timeout) || timeout <= 0) {
+                return;
+            }
+            timer = setTimeout(() => {
+                rej();
+            }, timeout);
+        });
+    };
+};
+
+module.exports = function(queue, count) {
+    if (_.isFinite(count)) {
+        const arr = [];
+        for (let i = 0; i < count; i++) {
+            arr.push(new QueueItem(queue));
+        }
+        return arr;
+    }
+    return new QueueItem(queue);
+};
--- tests/tests.js
+++ tests/tests.js
@@ -1,137 +1,42 @@
 'use strict';
-const _     = require('lodash');
-const Queue = require('../lib/index.js');
+const _            = require('lodash');
+const Queue        = require('../lib/index.js');
+const QueueItem     = require('./helpers/queueItem.js');
 
-const QueueFunc = function() {
-    let isExecuting = false;
-    this.isExecuting = () => {
-        return isExecuting;
-    };
+// Test 1:
 
-    let resolve;
-    this.resolved = false;
-    this.resolve = () => {
-        return new Promise((res, rej) => {
-            if (!isExecuting) {
-                rej();
-            }
-            if (!resolve) {
-                rej();
-            }
-            resolve();
-            isExecuting = false;
-            res();
-        });
-    };
-
-    let reject;
-    this.rejected = false;
-    this.reject = () => {
-        return new Promise((res, rej) => {
-            if (!isExecuting) {
-                rej();
-            }
-            if (!reject) {
-                rej();
-            }
-            reject();
-            isExecuting = false;
-            res();
-        });
-    };
-
-    this.pushable = () => {
-        return new Promise((res, rej) => {
-            resolve = res;
-            reject = rej;
-            isExecuting = true;
-        });
-    };
-};
-
-const getTestItems = (count) => {
-    const arr = [];
-    for (let i = 0; i < count; i++) {
-        arr.push(new QueueFunc());
-    }
-    return arr;
-};
-
-const testItems = getTestItems(3);
-const queue = new Queue(1);
-
-_.forEach(testItems, (testItem, index) => {
-    queue.push(testItem.pushable)
-        .then(() => {
-            console.log('Resolve at index:', index);
-            testItem.resolved = true;
-        })
-        .catch(() => {
-            console.log('Reject at index:', index);
-            testItem.rejected = true;
-        });
-});
+let queue = new Queue();
+let queueItems = QueueItem(queue, 4);
 
 Promise.resolve()
     .then(() => {
-        console.log('Concurrent', queue.getConcurrent());
-        console.log('Length', queue.getLength());
-        console.log('Item 0 executing', testItems[0].isExecuting());
-        console.log('Item 1 executing', testItems[1].isExecuting());
-        return testItems[0].resolve();
+        console.log(queue.getLength());
+        queueItems[0].push();
+        console.log(queue.getLength());
+        queueItems[1].push();
+        console.log(queue.getLength());
+        queueItems[2].push();
+        console.log(queue.getLength());
+        queueItems[3].push();
+        console.log(queue.getLength());
+        queueItems[0].resolve();
+        console.log('Resolving 0')
+        return queueItems[0].waitFor('finished');
     })
     .then(() => {
-        console.log('Item 0 resolved', testItems[0].resolved);
-        console.log('Item 0 rejected', testItems[0].rejected);
-        console.log('Concurrent', queue.getConcurrent());
-        console.log('Length', queue.getLength());
-        console.log('Item 0 executing', testItems[0].isExecuting());
-        console.log('Item 1 executing', testItems[1].isExecuting());
-    });
-
-setTimeout(() => {
-    console.log(testItems[1].isExecuting())
-}, 500)
-
-
-
-
-
-/*
-const test1 = new QueueFunc();
-
-
-
-queue.push()
-
-
-
-const startTime = Date.now();
-
-const thennable = (wait, msg) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log(msg, Date.now() - startTime);
-            resolve();
-        }, wait);
-    });
-};
-
-queue.push(thennable, 1000, 'Item1')
+        console.log(queueItems[0].isResolved())
+        console.log(queueItems[0].isRejected())
+        console.log(queue.getLength())
+        console.log(queueItems[0].isExecuting())
+        console.log(queueItems[1].isExecuting())
+        console.log('Pushing 1')
+        return queueItems[1].waitFor('executed');
+    })
     .then(() => {
-        console.log(queue.getLength());
-    });
-queue.push(thennable, 1000, 'Item2')
-    .then(() => {
-        console.log(queue.getLength());
-    });
-queue.push(thennable, 1000, 'Item3')
-    .then(() => {
-        console.log(queue.getLength());
-    });
-queue.push(thennable, 1000, 'Item4')
-    .then(() => {
-        console.log(queue.getLength());
-    });
+        console.log(queueItems[1].isResolved())
+        console.log(queueItems[1].isRejected())
+        console.log(queue.getLength())
+        console.log(queueItems[1].isExecuting())
+        console.log(queueItems[2].isExecuting())
+    })
 
-*/
